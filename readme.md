9b6769eda4833fc6c6b0a5162b7b5bd2830b2b2b
Andrea Ruggeri <hello@rggndr.it>
2024-09-03 16:31:11 +0200

    Initial commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+MongoDB
+andrea / AcV6bEplCunMFEFE
+
+mongodb+srv://andrea:c5jnLewKvJkh9ZYq@fantacalcio.qhvjn.mongodb.net/?retryWrites=true&w=majority&appName=fantacalcio
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+const socketIo = require('socket.io');
+require('dotenv').config();
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+  
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
